ab399f5274ef912e0d04dfbe925bc1dec256a38c
paradisemrch <tabooyu@gmail.com>
2021-04-02 17:55:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld msg="TarotKaka" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+@import "./assets/scss/main.scss";
+</style>
